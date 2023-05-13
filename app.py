4782e7d22c149f560f10fa0cb32e51fb96e35b7d
vaibhav Joshi <vaibhavgjoshi95@gmail.com>
2023-05-13 09:57:27 +0530

    Pipeline started

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from housing.logger import logging
 
 app = Flask(__name__)
 
 @app.route("/", methods=['GET','PUSH'])
 
 def index():
+    logging.info("We are testing logging project")
     return"Starting Machine LEarning Project"
 
 if __name__== "__main__":
